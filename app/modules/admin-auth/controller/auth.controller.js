932befdb310952bc9ec3c88c7cfa1edfcf62bd32
Phong Nguyễn <namduong.kh94@gmail.com>
2017-06-02 22:00:09 +0700

    Phong Nguyễn’s comment:

--- app/modules/admin-auth/controller/auth.controller.js
+++ app/modules/admin-auth/controller/auth.controller.js
@@ -5,7 +5,8 @@ exports.viewLogin = {
     handler: function(request, reply) {
         // console.log("request auth", request.auth);
         if (request.auth.isAuthenticated) {
-            return reply.redirect('/admin');
+            return reply("Đã đăng nhập");
+            // return reply.redirect('/admin');
         }
         return reply.view('admin-auth/views/signin', null, { layout: 'admin/layout-admin-login' });
     },
