08ceefcc8062de2cf4268dc8a7fe508d35059b20
Phong Nguyễn <namduong.kh94@gmail.com>
2017-06-02 21:21:35 +0700

    Phong Nguyễn’s comment:

--- app/modules/admin-dashboard/controller/dashboard.controller.js
+++ app/modules/admin-dashboard/controller/dashboard.controller.js
@@ -7,7 +7,8 @@ exports.index = {
     handler: function(request, reply) {
         // console.log("request", request.auth);
         if (!request.auth.credentials || !request.auth.credentials.scope.includes('admin')) {
-            return reply.redirect('/admin/dang-nhap');
+            return reply("Không có quyền truy cập");
+            // return reply.redirect('/admin/dang-nhap');
         }
         return reply.view('admin-dashboard/views/default', {}, { layout: 'admin/layout-admin' });
     },
