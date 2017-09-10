93ab2c8c785af58a6e8b774497121e6e85b550cd
Phong Nguyễn <namduong.kh94@gmail.com>
2017-09-10 23:19:21 +0700

    Phong Nguyễn’s comment:

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -554,6 +554,8 @@ var tips = {
     timeline: "Đây là danh sách các dòng thời gian bạn có quyền đăng bài viết trên tường. <br> Nhập tên của dòng thời gian (nhóm, trang) để tìm kiếm và thêm vào danh sách của bạn.",
     feed: "Dòng trạng thái là một bài viết có thể đăng lên tường dưới dạng một đoạn văn bản đơn thuần. <br> Bạn có thể chia sẻ một đường dẫn liên kết đến website khác kèm theo một dòng trạng thái.",
     album: "Album ảnh là một bài viết có thể đăng lên tường dưới dạng một đoạn văn bản đơn thuần kèm theo một hoặc nhiều hình ảnh.",
+    campaign: "Một chiến dịch là một công việc bạn dự định thực hiện, ví dụ: xuất bản trạng thái ABC lên dòng thời gian XYZ...<br>Bạn có thể chạy chiến dịch ngay sau khi tạo và lưu giữ lại để có thể chạy lại trong nhiều lần sau, giúp bạn tiết kiệm thời gian với các bài viết có nội dung lặp đi lặp lại.",
+    schedule: "Lịch trình là chức năng chạy chiến dịch hoàn toàn tự động. Chỉ cần lên lịch trình một lần, chiến dịch của bạn sẽ được chạy tự động và có thể lặp lại theo chu kỳ. Chức năng này giúp bạn tiết kiệm công sức, không cần phải online nhiều nhưng bài viết vẫn được xuất bản liên tục."
 };
 
 var placeholders = {
